7bfa35477d4a6b4f3ea1775007db18d857948da3
jayspringate <jay.springate@gmail.com>
2015-05-19 15:33:51 -0700

    add auth middleware, async bcrypt, unique emails

---
+++ test/auth-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('../server');
+process.env.MONGOLAB_URI = 'mongodb://localhost/players_test';
+
+var mongoose = require('mongoose');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+var Player = require('../models/Player');
+
+describe('fantasy baseball team players api', function () {
