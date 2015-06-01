f7e8cb2989db61faf6d37dfab0cff18c79ac3122
jayspringate <jay.springate@gmail.com>
2015-06-01 11:43:38 -0700

    committing changes to angular branch (has auth), will move to non-auth branch jayadd

--- app/js/client.js
+++ app/js/client.js
@@ -1,23 +1,35 @@
 'use strict';
 
-var greet = require('./greet');
+require('angular/angular');
 
-document.write(greet());
-var playerList = document.getElementById('playerList');
+var playersApp = angular.module('playersApp', []);
 
-var request = require('superagent');
+playersApp.controller('playersMainController', ['$scope', function($scope) {
+	$scope.greet = 'hello world';
+}])
 
-request
-	.get('/api/players')
-	.set('eat','qQJU7Ad8+bTDtcVYOebEkGybxRafHi58Ix6xcS+fVN7AT8PPFYBd8wpxau7o')
-	.end(function(err, res) {
-		if (err) return console.log(err);
-		var players = JSON.parse(res.text);
 
-		players.forEach(function(player) {
-			var playerEl = document.createElement('li');
-			playerEl.innerHTML = player.name;
-			playerList.appendChild(playerEl);
 
-		});
-	});
+
+
+// var greet = require('./greet');
+
+// document.write(greet());
+// var playerList = document.getElementById('playerList');
+
+// var request = require('superagent');
+
+// request
+// 	.get('/api/players')
+// 	.set('eat','qQJU7Ad8+bTDtcVYOebEkGybxRafHi58Ix6xcS+fVN7AT8PPFYBd8wpxau7o')
+// 	.end(function(err, res) {
+// 		if (err) return console.log(err);
+// 		var players = JSON.parse(res.text);
+
+// 		players.forEach(function(player) {
+// 			var playerEl = document.createElement('li');
+// 			playerEl.innerHTML = player.name;
+// 			playerList.appendChild(playerEl);
+
+// 		});
+// 	});
